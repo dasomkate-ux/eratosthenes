d9e091741fbc8dcf2d243049f7caf888bee551f9
Kate Chung <katenotkatie@berkeley.edu>
2025-10-24 17:25:35 +0000

    added some tests

--- sievetools/sieve.py
+++ sievetools/sieve.py
@@ -51,7 +51,13 @@ def sieve_fast(nmax):
     
     all_primes = []
 
-    if nmax == 2: 
+    if nmax == 1:
+        return []
+
+    elif nmax < 1:
+        raise ValueError("nmax cannot be less than 1")
+    
+    elif nmax == 2: 
         all_primes = [2]
 
     else:
